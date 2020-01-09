f95ede2af87d69a35ed04180a0324abe6c312e66
joselo01 <sjmujica@gmail.com>
2020-01-09 02:15:43 -0500

    update docs

---
+++ docs/precache-manifest.a3bd22b9efd9213ccd006005657e03c9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e761d5fe838ce70b9f60343cda73565f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0132988c685b2ae31f12",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "555e4bf039bd21ef95ba",
+    "url": "/static/css/main.7546bbc3.chunk.css"
+  },
+  {
+    "revision": "0132988c685b2ae31f12",
+    "url": "/static/js/2.e1b4406e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e1b4406e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "555e4bf039bd21ef95ba",
+    "url": "/static/js/main.0109ba79.chunk.js"
+  },
+  {
+    "revision": "a215d5eaf3fc059cb257",
+    "url": "/static/js/runtime-main.71fb29ad.js"
+  },
+  {
+    "revision": "e185af3d20d74082c9743e515a9de8a3",
+    "url": "/static/media/Illustration-app.e185af3d.svg"
+  },
+  {
+    "revision": "1afc90f3c6e46e472a7bf1d1f2ed9e2a",
+    "url": "/static/media/Illustration-inicio.1afc90f3.svg"
+  },
+  {
+    "revision": "077648076072eaff9b1767bfc0246a3d",
+    "url": "/static/media/Illustration-internas.07764807.svg"
+  },
+  {
+    "revision": "ddfd8e0a3a1dc784a8d1d343e626d7d9",
+    "url": "/static/media/Illustration_movil.ddfd8e0a.svg"
+  },
+  {
+    "revision": "9fa012de52761fd68893b708b8e42724",
+    "url": "/static/media/Logo-rimac.9fa012de.svg"
+  },
+  {
+    "revision": "bd83fa4a7b5ef8e11eb0549997ae7a57",
+    "url": "/static/media/back.bd83fa4a.svg"
+  },
+  {
+    "revision": "71ee30f7c4d38f41b2b4559f7157a26d",
+    "url": "/static/media/checkv.71ee30f7.svg"
+  },
+  {
+    "revision": "07de3504b4a334e5d1c9aa044f6507b0",
+    "url": "/static/media/ic_add.07de3504.svg"
+  },
+  {
+    "revision": "829bb1e988b2936721ed955948e4a917",
+    "url": "/static/media/ic_medicine.829bb1e9.svg"
+  },
+  {
+    "revision": "64f0af43541e00643533dec53992bf67",
+    "url": "/static/media/ic_next_disabled.64f0af43.svg"
+  },
+  {
+    "revision": "798caaad60e8426bb5b5dcf4540a5548",
+    "url": "/static/media/ic_next_enabled.798caaad.svg"
+  },
+  {
+    "revision": "66664f337ef0faa22556a49ebb166b34",
+    "url": "/static/media/ic_tooltip.66664f33.svg"
+  },
+  {
+    "revision": "73b9e48ce2a1f3dc390b1f39282247db",
+    "url": "/static/media/visa-azul.73b9e48c.svg"
+  },
+  {
+    "revision": "9629e2446ae979ff3047c1e6588b80f8",
+    "url": "/static/media/visa.9629e244.svg"
+  }
+]);
